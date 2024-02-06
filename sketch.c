c92c959d2a3a7b4408477470b01bcb8237c7dbdb
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2024-02-06 19:50:20 +0530

    updated with menu

--- sketch.c
+++ sketch.c
@@ -8,25 +8,28 @@
 #define DISPLAY_RESET -1
 #define DISPLAY_ADDRESS 0x3C
 
-Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &Wire, DISPLAY_RESET);
+Adafruit_SSD1306 display(DISPLAY_WIDTH,DISPLAY_HEIGHT,&Wire,DISPLAY_RESET);
 
 #define BUZZER 5
 #define LED_1 15
 #define PB_CANCEL 34
+#define PB_OK 32
+#define PB_UP 35
+#define PB_DOWN 33
 
 int days = 0;
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
 
-unsigned long time_now = 0;
-unsigned long time_last = 0;
+unsigned long  time_now = 0;
+unsigned long  time_last = 0;
 
 bool alarm_enabled = true;
 int n_alarms = 2;
-int alarm_hours[] = {0, 1};
-int alarm_minutes[] = {0, 10};
-bool alarm_triggered[] = {false, false};
+int alarm_hours[] = {0,1};
+int alarm_minutes[] = {1,10};
+bool alarm_triggered[] = {false,false};
 
 int n_notes = 8;
 int C = 262;
@@ -38,134 +41,190 @@ int A = 440;
 int B = 494;
 int C_H = 523;
 
-int notes[] = {C, D, E, F, G, A, B, C_H};
+int notes[] = {C,D,E,F,G,A,B,C_H};
 
-void setup()
-{
+int current_mode = 0;
+int max_mode = 4;
+String modes[] = {"1 - Set Time","2 - Set Alarm 1","3 - Set Alarm 2","4 - Disable Alarm"};
+
+void setup() {
   // put your setup code here, to run once:
   pinMode(BUZZER, OUTPUT);
   pinMode(LED_1, OUTPUT);
   pinMode(PB_CANCEL, INPUT);
+  pinMode(PB_OK,INPUT);
+  pinMode(PB_UP,INPUT);
+  pinMode(PB_DOWN,INPUT);
+
 
   Serial.begin(9600);
   Serial.println("MediBox is Initializing!");
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS))
-  {
+  if(!display.begin(SSD1306_SWITCHCAPVCC,DISPLAY_ADDRESS)){
     Serial.println(F("Medibox SSD1306 Display Allocation Failed"));
-    for (;;)
-      ;
+    for(;;);
   }
 
   // Displaying buffer on the screen
   display.display();
   delay(2000);
   display.clearDisplay();
-  print_line("Welcome to MediBox!", 0, 0, 2);
+  print_line("Welcome to MediBox!",0,0,2);
 
   delay(2000);
+
+
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
   update_time_with_check_alarm();
+  if(digitalRead(PB_OK) == LOW){
+    delay(200);
+    go_to_menu();
+  }
+
+  
 }
 
-void print_line(String text, int column, int row, int text_size)
-{
-  // Clearing the buffer
-  // display.clearDisplay();
+void print_line(String text,int column,int row,int text_size){
+   // Clearing the buffer
+  //display.clearDisplay();
+
 
   display.setTextSize(text_size);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(column, row); // Column,Row
+  display.setCursor(column,row); // Column,Row
   display.println(text);
 
   display.display();
 }
 
-void print_time_now(void)
-{
+void print_time_now(void){
 
   display.clearDisplay();
   print_line(String(days), 0, 0, 2);
   print_line(":", 20, 0, 2);
-  print_line(String(hours), 30, 0, 2);
+  print_line(String (hours), 30, 0, 2);
   print_line(":", 50, 0, 2);
-  print_line(String(minutes), 60, 0, 2);
+  print_line(String(minutes),60, 0, 2);
   print_line(" :", 80, 0, 2);
-  print_line(String(seconds), 90, 0, 2);
+  print_line(String(seconds),90, 0, 2);
+
 }
 
-void update_time(void)
-{
-  time_now = millis() / 1000;
+void update_time(void){
+  time_now = millis()/1000;
 
   seconds = time_now - time_last;
 
-  if (seconds >= 60)
-  {
+  if(seconds >= 60){
     minutes += 1;
     time_last += 60;
   }
 
-  if (minutes == 60)
-  {
+  if(minutes == 60){
     hours += 1;
     minutes = 0;
   }
 
-  if (hours == 24)
-  {
+  if(hours == 24){
     days += 1;
     hours = 0;
   }
+
 }
 
-void update_time_with_check_alarm(void)
-{
+void update_time_with_check_alarm(void){
   print_time_now();
   update_time();
 
-  if (alarm_enabled == true)
-  {
-    for (int i = 0; i < n_alarms; i++)
-    {
-      if (alarm_triggered[i] == false && alarm_hours[i] == hours && alarm_minutes[i] == minutes)
-      {
+
+  if(alarm_enabled == true){
+    for(int i=0;i<n_alarms;i++){
+      if(alarm_triggered[i] == false && alarm_hours[i] == hours && alarm_minutes[i] == minutes){
         ring_alarm();
         alarm_triggered[i] = true;
       }
     }
   }
+
 }
 
-void ring_alarm(void)
-{
+void ring_alarm(void){
   display.clearDisplay();
-  print_line("Medicine Time!", 0, 0, 2);
-  digitalWrite(LED_1, HIGH);
+  print_line("Medicine Time!",0,0,2);
+  digitalWrite(LED_1,HIGH);
 
   bool break_happened = false;
 
-  while (digitalRead(PB_CANCEL) == HIGH && break_happened == false)
-  {
+  while(digitalRead(PB_CANCEL) == HIGH && break_happened == false){
     // Ring the Buzzer
-    for (int i = 0; i < n_notes; i++)
-    {
-      if (digitalRead(PB_CANCEL) == LOW)
-      {
+    for(int i=0;i<n_notes;i++){
+      if(digitalRead(PB_CANCEL) == LOW){
         delay(200);
         break_happened = true;
         break;
       }
-      tone(BUZZER, notes[i]);
+      tone(BUZZER,notes[i]);
       delay(500);
       noTone(BUZZER);
       delay(2);
     }
   }
-  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_1,LOW);
   display.clearDisplay();
+
+}
+
+void go_to_menu(void){
+  display.clearDisplay();
+  print_line("Menu",0,2,2);
+  delay(1000);
+  while(digitalRead(PB_CANCEL) == HIGH){
+    display.clearDisplay();
+    print_line(modes[current_mode],0,2,2);
+    delay(1000);
+
+    int pressed = wait_for_button_press();
+
+    if(pressed == PB_UP){
+      delay(200);
+      current_mode += 1;
+      current_mode = current_mode % max_modes;
+    }
+    else if(pressed == PB_DOWN){
+      delay(200);
+      current_mode -= 1;
+      if(current_mode <= -1){
+        current_mode = max_modes - 1;
+      }
+    }
+    else if(pressed == PB_CANCEL){
+      delay(200);
+      break;
+    }
+
+  }
+}
+
+int wait_for_button_press(){
+  while(true){
+    if(digitalRead(PB_UP) == LOW){
+      delay(200);
+      return PB_UP;
+    }
+    else if(digitalRead(PB_DOWN) == LOW){
+      delay(200);
+      return PB_DOWN;
+    }
+    else if(digitalRead(PB_OK) == LOW){
+      delay(200);
+      return PB_OK;
+    }
+    else if(digitalRead(PB_CANCEL) == LOW){
+      delay(200);
+      return PB_CANCEL;
+    }
+  }
 }
